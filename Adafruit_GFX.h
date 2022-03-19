63a5c4fbdc3e93b5222bd4fb665f640e70829025
mron <ronmoreland@att.net>
2022-03-18 23:18:29 -0700

    Added modulo to drawBitMap

--- Adafruit_GFX.h
+++ Adafruit_GFX.h
@@ -83,9 +83,9 @@ public:
   void drawBitmap(int16_t x, int16_t y, const uint8_t bitmap[], int16_t w,
                   int16_t h, uint16_t color, uint16_t bg);
   void drawBitmap(int16_t x, int16_t y, uint8_t *bitmap, int16_t w, int16_t h,
-                  uint16_t color);
+                  uint16_t color );
   void drawBitmap(int16_t x, int16_t y, uint8_t *bitmap, int16_t w, int16_t h,
-                  uint16_t color, uint16_t bg);
+                  uint16_t color, uint16_t bg , uint16_t modulo);
   void drawXBitmap(int16_t x, int16_t y, const uint8_t bitmap[], int16_t w,
                    int16_t h, uint16_t color);
   void drawGrayscaleBitmap(int16_t x, int16_t y, const uint8_t bitmap[],
